0eebaac3253b25013a648ea4803c99d1c7b5f29e
Maxim <101442731+nyonnn@users.noreply.github.com>
2024-10-29 20:46:41 -0700

    close #5

---
+++ personal-website/src/components/experiencesection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProjectsSection.css';
+
+function ProjectsSection() {
+  return (
+    <section className="projects-section">
+      <div className="projects-card">
+        <h2>My Projects</h2>
+        <p>Subtitle</p>
+        <ul>
+          <li>Neo Suite</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default ProjectsSection;
